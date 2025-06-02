f33297d6f2e1d6b457d59a24a9e565dfda1cd52a
Pravalika <112800726+Pravalika-27@users.noreply.github.com>
2025-06-02 22:17:01 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,8 +1,8 @@
+\// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nxt-delivery/',  // ✅ use only the repo name with leading/trailing slashes
+  base: '/nxt-delivery/',  // ✅ this must match your repo name
   plugins: [react()],
 })
